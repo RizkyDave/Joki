f89b49b373f354af8a72651bde6b402e7d2a8535
DappyNetwork <92771259+Dappy-Net@users.noreply.github.com>
2023-10-28 15:25:29 +0700

    JavaScript For The Slider image 🖼

---
+++ script.js
@@ -0,0 +1,24 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
+
+const filterText = document.getElementById('filterText');
+const filterSelect = document.getElementById('filterSelect');
+
+filterSelect.addEventListener('change', () => {
+  const selectedOption = filterSelect.options[filterSelect.selectedIndex];
+  filterText.textContent = `Filter ${selectedOption.textContent} v`;
+});
+
